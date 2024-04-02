3ff69ff1e0775f7427137a73801cec4baf2e91a8
dasmieszneznaczki <erickdiogo2004@gmail.com>
2024-04-02 17:17:37 -0300

    ✨ Pass mode selectors through MQTT

--- main.ino
+++ main.ino
@@ -14,12 +14,12 @@ const int PINO_SNAKE4 = 35;
 const int PINO_SNAKE5 = 32;
 const int PINO_SNAKE6 = 33;
 
-
+ 
 // APPLE_POS
 
 const int PINO_APPLE1 = 23;
 const int PINO_APPLE2 = 22;
-const int PINO_APPLE3 = 3;
+const int PINO_APPLE3 = 5;
 const int PINO_APPLE4 = 21;
 const int PINO_APPLE5 = 19;
 const int PINO_APPLE6 = 18;
@@ -28,9 +28,21 @@ const int PINO_APPLE6 = 18;
 
 const int PINO_STATE1 = 27;
 const int PINO_STATE2 = 13;
-const int PINO_STATE3 = 17;
+const int PINO_STATE3 = 16;
 const int PINO_STATE4 = 4;
 const int PINO_STATE5 = 15;
+
+// VELOCITY
+
+const int PINO_VEL = 14;
+
+// MODE
+
+const int PINO_MODE = 12;
+
+// DIFF
+
+const int PINO_DIFF = 2;
 
 // const int PINO_3V3 = 15; 
 
@@ -43,14 +55,14 @@ const int PINO_COMEU_MACA = 25; // FPGA signal
 const int PINO_BUZZER = 26; // Buzzer pin
 
 // LED
-const int PINO_LED = 2; // Internal LED for debugging
+// const int PINO_LED = 2; // Internal LED for debugging
 
 const int PWM_CHANNEL = 0; // PWM Channel for buzzer
 
 // Network setup variables
-const char* ssid = "NOME_WIFI";
-const char* password = "SENHA_WIFI";
-const char* mqtt_server = "IP_DO_BROKER";
+const char* ssid = "Das";
+const char* password = "12345678";
+const char* mqtt_server = "192.168.135.253";
 const int connection_port = 1883;
 
 //create wifi client 
@@ -104,7 +116,11 @@ void setup() {
 
   //pinMode(PINO_3V3, OUTPUT);
 
-  pinMode(PINO_LED, OUTPUT); 
+  //pinMode(PINO_LED, OUTPUT);
+
+  pinMode(PINO_VEL, INPUT); 
+  pinMode(PINO_MODE, INPUT); 
+  pinMode(PINO_DIFF, INPUT); 
   pinMode(PINO_COMEU_MACA, INPUT);   
 
 
@@ -126,6 +142,8 @@ void setup_wifi() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.println("Trying to connect to wifi \n");
+    Serial.println(ssid);
+    Serial.println(password);
   }
 
   Serial.println("");
@@ -187,11 +205,14 @@ void loop() {
     }
 
     int new_comeu_maca = digitalRead(PINO_COMEU_MACA);
+    
+    int vel_select = digitalRead(PINO_VEL);
+    int mode_select = digitalRead(PINO_MODE);
+    int diff_select = digitalRead(PINO_DIFF);
 
-    if (new_comeu_maca != comeu_maca || 
-        strcmp(new_snake_pos_string.c_str(), snake_pos_string.c_str()) != 0 ||
-        strcmp(new_apple_pos_string.c_str(), apple_pos_string.c_str()) != 0 ||
-        strcmp(new_state_string.c_str(), state_string.c_str()) != 0) {
+    if (new_comeu_maca != comeu_maca 
+    // || strcmp(new_snake_pos_string.c_str(), snake_pos_string.c_str()) != 0 || strcmp(new_apple_pos_string.c_str(), apple_pos_string.c_str()) != 0 || strcmp(new_state_string.c_str(), state_string.c_str()) != 0
+    ) {
       updated = true;
     }
     else {
@@ -199,11 +220,11 @@ void loop() {
     }
 
     int melody[] = {
-        440, 470, 500, 395, 410, 440, 470, 500
+        440, 100, 850
     };
 
     int tempo[] = {
-        50, 50, 50, 50, 50, 50, 50, 50
+        50, 50, 50
     };
 
     if (!client.connected()) {
@@ -213,7 +234,7 @@ void loop() {
     
     client.loop();
 
-  if (updated) {
+  //if (updated) {
     Serial.println("comeu_maca: ");
     Serial.println(new_comeu_maca);
 
@@ -225,26 +246,28 @@ void loop() {
     client.publish("snake_game/apple_pos", apple_pos_string.c_str());
     client.publish("snake_game/state", state_string.c_str());
 
+    client.publish("snake_game/vel_selector", String(vel_select).c_str());
+    client.publish("snake_game/mode_selector", String(mode_select).c_str());
+    client.publish("snake_game/diff_selector", String(diff_select).c_str());
+
     if (new_comeu_maca == 1) {
       comeu_maca = 1;
-      digitalWrite(PINO_LED, HIGH);
+      // digitalWrite(PINO_LED, HIGH);
       client.publish("snake_game/comeu_maca", "1");
 
       for (int i = 0; i < sizeof(melody) / sizeof(melody[0]); i++)  {
         ledcWrite(PWM_CHANNEL, melody[i]);
         delay(tempo[i] * 1.30);             // Pequeno delay para espaçar as notas
         ledcWrite(PWM_CHANNEL, 0);          // Desliga o buzzer ao final de cada nota
-        delay(25);                          // Pequeno delay para evitar cliques entre as notas
+        if (i != sizeof(melody) / sizeof(melody[0]) - 1)
+          delay(25);                          // Pequeno delay para evitar cliques entre as notas
       }
 
     }
     else {
       comeu_maca = 0;
-      digitalWrite(PINO_LED, LOW);
+      // digitalWrite(PINO_LED, LOW);
       client.publish("snake_game/comeu_maca", "0");
     }
-  }
-  
-  delay(50); // Pausa na musica por 50 ms
-
+  //}
 }
